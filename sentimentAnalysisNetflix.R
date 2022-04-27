f685e6d02be229abdbd951a10cc71492a817bf7f
Matt London <mattlondon57@gmail.com>
2022-04-26 22:56:17 -0400

    Add various plots and means as well as data exports

--- sentimentAnalysisNetflix.R
+++ sentimentAnalysisNetflix.R
@@ -21,6 +21,8 @@ netflixData$text[1:4]
 
 # Do the same but with different method
 sentAfinn <- get_sentiment(netflixData$text, method = 'afinn')
+plot(sentAfinn, type = "p")
+View(sentAfinn)
 sentAfinn[1:5]
 
 # Try third method
@@ -28,6 +30,8 @@ sentNrc <- get_sentiment(netflixData$text, method = "nrc")
 
 # Try fourth method
 sentBing <- get_sentiment(netflixData$text, method = "bing")
+get_sentiment_dictionary("bing")
+plot(sentBing, type = "p")
 
 # Merge the data set to include the sentiment analysis
 # cbind adds new columns to a data frame
@@ -54,6 +58,7 @@ write_xlsx(noDupSentiNetflix, "sentiNetflix.xlsx")
 
 # Make a plot
 plot(sentSyuzhet, type = "p")
+mean(sentSyuzhet)
 
 # View NRC dictionary
 View(get_sentiment_dictionary("nrc"))
@@ -83,7 +88,55 @@ write.table(noDupSentiNetflix$text, file = "netflixTweets.txt", row.names = FALS
 stock <- grepl("stock", noDupSentiNetflix$text, ignore.case = FALSE)
 table(stock) # Gives sum of values
 
+# Grab all the values that reference the stock
+netflixStock <- sentiNetflix[stock, ]
+stock_nrc <- get_nrc_sentiment(netflixStock$text)
+View(netflixStock)
+
+# Build a plot of the stock referenced tweets
+png("netflixStock_nrc_senti_graph.png", width = 12, height = 8, units = "in", res = 300)
+barplot(sort(colSums(prop.table(stock_nrc))),
+        horiz = FALSE,
+        cex.names = 0.7,
+        las = 1,
+        main = "Emotions in Netflix stock tweets",
+        ylab = "Percentage",
+        xlab = "Emotion")
+dev.off()
+
 # Install ggplot
 install.packages("ggplot2")
 library("ggplot2")
 
+
+# Grab tweets about specifically Netflix's stock symbol
+nflxStockData <- search_tweets("nflx",n=2000,include_rts = FALSE,lang="en")
+
+# Run NRC sentiment analysis on those tweets
+nrc_stock_data <- get_nrc_sentiment(nflxStockData$text)
+
+# Build the plot
+png("nflx_nrc_senti_graph.png", width = 12, height = 8, units = "in", res = 300)
+barplot(sort(colSums(prop.table(stock_nrc))),
+        horiz = FALSE,
+        cex.names = 0.7,
+        las = 1,
+        main = "Emotions in NFLX stock tweets",
+        ylab = "Percentage",
+        xlab = "Emotion")
+dev.off()
+
+
+# Get NFLX syuzhet analtsis
+nflxSyuzhet <- get_sentiment(nflxStockData$text, method = 'syuzhet')
+nflxAfinn <- get_sentiment(nflxStockData$text, method = 'afinn')
+nflxBing <- get_sentiment(nflxStockData$text, method = 'bing')
+
+sentiNflx <- cbind(nflxStockData, nflxSyuzhet, nflxAfinn, nflxBing)
+
+write_xlsx(sentiNflx, "nflxSenti.xlsx")
+
+# Plot syuzhet for NFLX
+plot(nflxSyuzhet, type = "p")
+mean(nflxSyuzhet)
+summary(nflxSyuzhet)
